813ac6d4a64845eb97046e1473d6cbc8ae6baaa1
Sumit Dey <sumitdey0007@gmail.com>
2021-05-24 23:59:57 +0200

    recap done

--- src/main/java/security/Recaptcha.java
+++ src/main/java/security/Recaptcha.java
@@ -14,7 +14,7 @@ public class Recaptcha {
     public static boolean validateHuman(String retoken) throws IOException{
         boolean isVerified = false;
         String secret = "6LfPttgaAAAAAD-rq4dfmTAG-hoEWSBA4uSf5EHK" ;
-        String url = "https://www.google.com/recaptcha/api/siteverify?secret=6LfPttgaAAAAAD-rq4dfmTAG-hoEWSBA4uSf5EHK&response=" + retoken;
+        String url = "https://www.google.com/recaptcha/api/siteverify?secret=6LfliNgaAAAAANykS4dsxm7vEhSoUdAz1q8C2UpS&response=" + retoken;
         String response = HttpUtils.postData(url);
         RecapDTO recapDTO = GSON.fromJson(response, RecapDTO.class);
         System.out.println(recapDTO.success);
